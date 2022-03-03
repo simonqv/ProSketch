56de2eb99726e79d926420a014978ec41c8ae74b
celinas <celinas@kth.se>
2022-03-03 16:49:45 +0100

    Spawner

---
+++ MVK/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+   
+    public void Spawn()
+    {
+        Instantiate(prefab);
+    }
+}
